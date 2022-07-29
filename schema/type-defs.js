830e3b02f9d40f8c2e563684243a52e68430e53f
kristenwood123 <kristenwood123@gmail.com>
2022-07-29 14:33:35 -0700

    Queried Users List

--- schema/type-defs.js
+++ schema/type-defs.js
@@ -1,16 +1,17 @@
-const { gql } = require("apollo-server")
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
     type User {
-        id:ID!
+        id: ID!
         name: String!
         username: String!
         age: Int!
         nationality: String!
-
     }
+
     type Query {
         users: [User!]!
-        friends
     }
-`
+`;
+
+module.exports = { typeDefs };
